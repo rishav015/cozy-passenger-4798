a05ff6233203be811ca8565d7e940f98a7f2db51
Rishav Choudhary <78720189+rishav015@users.noreply.github.com>
2022-08-26 21:46:08 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -33,3 +33,81 @@ document.querySelector(".gotologin").addEventListener("click", function(){
 document.querySelector(".gotocart").addEventListener("click", function(){
     window.location.href = "cart.html";
 });
+
+
+// document.querySelector(".creatacc").innerHTML = null;
+
+document.querySelector(".regbtn").addEventListener("click",gonext);
+function gonext(){
+    document.getElementById("login-form").style.display = "none";
+    document.querySelector(".register").style.display = "block";
+    document.querySelector(".loginbtn").style.display = "block";
+    document.querySelector(".regbtn").style.display = "none";
+}
+
+document.querySelector(".loginbtn").addEventListener("click",gonext1);
+function gonext1(){
+    document.getElementById("login-form").style.display = "block";
+    document.querySelector(".register").style.display = "none";
+    document.querySelector(".loginbtn").style.display = "none";
+    document.querySelector(".regbtn").style.display = "block";
+
+}
+
+// register user function
+
+document.querySelector(".sub2").addEventListener("click",addData);
+    
+    var userData = JSON.parse(localStorage.getItem("userDatabase")) || [];
+    function addData(){
+        var fstname=document.querySelector("#fname").value;
+        var lstname=document.querySelector("#lname").value;
+        var emlname=document.querySelector("#eml2").value;
+        var passname=document.querySelector("#pass2").value;
+   
+    var userCard={
+        firstname:fstname,
+        lastname:lstname,
+        emailname:emlname,
+        passwordname:passname
+    };
+    userData.push(userCard);
+    localStorage.setItem("userDatabase", JSON.stringify(userData));
+
+    if(fstname == "" || lstname == "" || emlname == "" || passname == ""){
+        if(passname.length <= 5){
+            alert("Please fill all information");
+        }
+        else{
+            alert("Please fill all information");
+        }
+    }
+    else {
+        alert("Register successfull");
+    }
+}
+
+// user login function
+
+document.querySelector(".sub").addEventListener("click", addDetails);
+
+    function addDetails(event){
+        event.preventDefault();
+        var email = document.querySelector("#eml").value;
+        
+        var passwor = document.querySelector("#pass").value;
+
+        var userdata = JSON.parse(localStorage.getItem("userDatabase"));
+        var flag = 0;
+
+        for(var i=0; i<userdata.length; i++){
+            if((email == userdata[i].emailname) && (passwor == userdata[i].passwordname)){
+                window.location.href = "index.html";
+                break;
+            }
+            else{
+                alert("Invalid Email or Password");
+                break;
+            }
+        }
+    }
